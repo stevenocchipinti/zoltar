fef678f6a1c14dc98fc2753eee6ac433588e120e
Steve Occhipinti <dev@stevenocchipinti.com>
2022-09-26 19:53:15 +1000

    Add a startup sound

--- play.py
+++ play.py
@@ -3,22 +3,23 @@ import vlc
 import random
 import os
 
-AUDIO_DIR = "files"
-
 media_player = vlc.MediaPlayer()
 files = os.listdir("files")
 
-def button_callback(channel):
-    print("Button pressed!")
-    file = random.choice(files)
+def play(file):
     print("Playing file: " + file)
-    media_player.set_media(vlc.Media(AUDIO_DIR + "/" + file))
+    media_player.set_media(vlc.Media(file))
     media_player.play()
 
+def button_callback(channel):
+    play("files/" + random.choice(files))
+
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initially pulled low (off)
 GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
+play("boot.mp3")
 input("Press any key to quit")
 
 GPIO.cleanup()
