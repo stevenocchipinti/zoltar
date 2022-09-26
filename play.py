5dbf60f93924c62ac7f23208ca671fe93e814805
Steve Occhipinti <dev@stevenocchipinti.com>
2022-09-26 20:43:49 +1000

    Drive a servo, use signals and all the things

--- play.py
+++ play.py
@@ -1,25 +1,36 @@
+#!/use/bin/env python
 import RPi.GPIO as GPIO
 import vlc
 import random
 import os
+from signal import pause
 
 media_player = vlc.MediaPlayer()
-files = os.listdir("files")
 
-def play(file):
-    print("Playing file: " + file)
-    media_player.set_media(vlc.Media(file))
+files_path = os.path.join(os.path.dirname(_file_), "files")
+files = os.listdir(files_path)
+
+def play(filename):
+    print("Playing file: " + filename)
+    full_path = os.path.join(os.path.dirname(_file_), filename)
+    media_player.set_media(vlc.Media(full_path))
     media_player.play()
 
 def button_callback(channel):
     play("files/" + random.choice(files))
 
-GPIO.setmode(GPIO.BOARD)
+try:
+    GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initially pulled low (off)
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initially pulled low (off)
+    GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
-play("boot.mp3")
-input("Press any key to quit")
+    play("boot.mp3")
+    pause()
 
-GPIO.cleanup()
+except KeyboardInterrupt:
+    print("Thanks for playing")
+
+finally:
+    print("Cleaning up...")
+    GPIO.cleanup()
