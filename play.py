6a9f9a0b5b4497300dd9352558f91b579ea9b141
Steve Occhipinti <dev@stevenocchipinti.com>
2022-09-27 15:30:58 +1000

    wip: Attempt to do stuff while the audio is playing
    This will eventually be moving the mechanical jaw via a servo

--- play.py
+++ play.py
@@ -3,18 +3,27 @@ import RPi.GPIO as GPIO
 import vlc
 import random
 import os
-from signal import pause
-
-media_player = vlc.MediaPlayer()
+import time
+import signal
 
 files_path = os.path.join(os.path.dirname(_file_), "files")
 files = os.listdir(files_path)
+
+media_player = vlc.MediaPlayer()
+
+def move_jaw_while_playing(delay=0.5):
+    time.sleep(delay)
+    while media_player.is_playing():
+        print(".", end="", flush = True)
+        sleep(0.3)
+    print(";")
 
 def play(filename):
     print("Playing file: " + filename)
     full_path = os.path.join(os.path.dirname(_file_), filename)
     media_player.set_media(vlc.Media(full_path))
     media_player.play()
+    move_jaw_while_playing()
 
 def button_callback(channel):
     play("files/" + random.choice(files))
@@ -26,7 +35,8 @@ try:
     GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
 
     play("boot.mp3")
-    pause()
+
+    signal.pause()
 
 except KeyboardInterrupt:
     print("Thanks for playing")
