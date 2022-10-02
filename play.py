389a8d8b218db75cb920820025eca5c7aaff899d
Steve Occhipinti <dev@stevenocchipinti.com>
2022-10-02 15:18:10 +1100

    Basic servo driving test code

--- play.py
+++ play.py
@@ -10,6 +10,12 @@ files_path = os.path.join(os.path.dirname(__file__), "files")
 files = os.listdir(files_path)
 
 media_player = vlc.MediaPlayer()
+servo = None
+
+def move_jaw(angle):
+    servo.ChangeDutyCycle(2+(angle/18))
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(0)
 
 def move_jaw_while_playing(delay=0.5):
     time.sleep(delay)
@@ -26,13 +32,18 @@ def play(filename):
     move_jaw_while_playing()
 
 def button_callback(channel):
-    play("files/" + random.choice(files))
+    move_jaw(random.randint(0,180))
+    # play("files/" + random.choice(files))
 
 try:
     GPIO.setmode(GPIO.BOARD)
 
     GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # initially pulled low (off)
     GPIO.add_event_detect(10, GPIO.RISING, callback=button_callback)
+
+    GPIO.setup(8, GPIO.OUT)
+    servo = GPIO.PWM(8 ,50)
+    servo.start(0)
 
     play("boot.mp3")
 
@@ -43,4 +54,5 @@ except KeyboardInterrupt:
 
 finally:
     print("Cleaning up...")
+    servo.stop()
     GPIO.cleanup()
